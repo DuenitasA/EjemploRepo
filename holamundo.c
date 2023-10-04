a53016516cf23f315a66c07db821dadbc10b0291
Alejandro Duenas <alejandro.duenas@udla.ecu.ec>
2023-10-04 16:13:35 -0500

    El programa esta completo con las actualizaciones correspondientes

--- holamundo.c
+++ holamundo.c
@@ -1,30 +1,39 @@
 #include <stdio.h>
 
-int main(void){
-
+int main(void)
+{
     int numeroprimero;
-    int contador =0 ;
-    printf("Ingrese un numero para comprobrar si es primo o no.\n");
-    scanf("%d", &numeroprimero);
-
-    for (int i = 1; i <= numeroprimero; i++)
+    int contador = 0;
+    int opcion;
+    printf("Bienvenido al programa de calculo de numeros primos.\n");
+    do
     {
-        if (numeroprimero % i == 0)
+
+        printf("Ingrese un numero para comprobrar si es primo o no.\n");
+        scanf("%d", &numeroprimero);
+
+        for (int i = 1; i <= numeroprimero; i++)
         {
-            contador ++;
+            if (numeroprimero % i == 0)
+            {
+                contador++;
+            }
         }
-        
-    }
-    if (contador >2  )
-    {
-        printf("El numero %d no es primo.\n", numeroprimero);
+        if (contador > 2)
+        {
+            printf("El numero %d no es primo.\n", numeroprimero);
+        }
+        else
+        {
+            printf("El numero %d es primo.\n", numeroprimero);
+        }
+        printf("Desea volver a calcular un numero? Escriba 1 para volver a calcular y cualquier otro numero para salir del programa.\n");
+        scanf("%d", &opcion);
+        if (opcion == 1)
+        {
+            contador = 0;
+        }
 
-    }else{
-        printf("El numero %d es primo.\n", numeroprimero);
-
-    }
-    
-    
-
+    } while (opcion == 1);
     return 0;
 }
